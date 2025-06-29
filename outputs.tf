6cf7f97dd03f05a2f4db0143fda19102b3f9e33e
carlos <carlosftru@gmail.com>
2025-06-28 23:54:04 -0600

    added outputs

---
+++ outputs.tf
@@ -0,0 +1,44 @@
+output "front-office-server" {
+  description = "front office web service plan name"
+  value = azurerm_service_plan.front-office-ecommerce.name
+}
+
+output "front-office-ui" {
+  description = "UI web app"
+  value = azurerm_linux_web_app.ui.name
+}
+
+output "front-office-api" {
+  description = "API web app"
+  value = azurerm_linux_web_app.api.name
+}
+
+output "cache-redis" {
+  description = "Cache database"
+  value = azurerm_redis_cache.cache.name
+}
+
+output "queue-storage" {
+  description = "storage for queues"
+  value = azurerm_storage_account.storage.name
+}
+
+output "notification-queue" {
+  description = "async notification operations queue"
+  value = azurerm_storage_queue.notifications.name
+}
+
+output "payments-queue" {
+  description = "async payments operations queue"
+  value = azurerm_storage_queue.payments.name
+}
+
+output "notifications-serverless" {
+  description = "serverless notifications function"
+  value = azurerm_linux_function_app.notifications.name
+}
+
+output "payments-serverless" {
+  description = "serverless payments function"
+  value = azurerm_linux_function_app.payments.name
+}
