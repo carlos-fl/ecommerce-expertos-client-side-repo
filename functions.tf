c066324328b107e41e4be5edb715180be4031e4b
carlos <carlosftru@gmail.com>
2025-07-01 22:21:10 -0600

    added neccesary config in functions apps

--- functions.tf
+++ functions.tf
@@ -4,6 +4,8 @@ resource "azurerm_linux_function_app" "notifications" {
   location = var.location
   service_plan_id = azurerm_service_plan.front-office-ecommerce.id
 
+  storage_account_name = azurerm_storage_account.storage.name
+
   site_config {
     application_stack {
       python_version = "3.13"
@@ -16,6 +18,8 @@ resource "azurerm_linux_function_app" "payments" {
   resource_group_name = azurerm_resource_group.rg.name
   location = var.location
   service_plan_id = azurerm_service_plan.front-office-ecommerce.id
+
+  storage_account_name = azurerm_storage_account.storage.name
 
   site_config {
     application_stack {
